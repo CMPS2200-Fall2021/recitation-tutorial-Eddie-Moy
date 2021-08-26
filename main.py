712691988fbe5f90a78283c482cc4c0fed9a6ff0
Eddie-Moy <emoy@tulane.edu>
2021-08-26 10:04:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 def sum_of_squares(a):
-	sum = 0
-	for i in a
-		sum += a[i] * a[i]
+   sum = 0
+   for i in range(len(a)):
+      toAdd = a[i]**2
+      sum += toAdd
+   return sum
 
+
+	
 def test_one():
-    assert sum_of_squares([1,2,3]) == 14
+   assert sum_of_squares([1,2,3]) == 14
+
+def test_two():
+   assert sum_of_squares([4,4,4]) == 48
+
+test_one()
+test_two()
